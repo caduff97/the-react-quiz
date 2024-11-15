57b2395bd11245830cd2d9384012d9b13385b6bf
cadueff <kadufernandes.kf@gmail.com>
2024-11-14 21:25:12 -0800

    include Context Provider

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,6 +1,7 @@
-import { type } from "@testing-library/user-event/dist/type";
+import { useQuiz } from "../contexts/QuizContext";
 
-function Options({ question, dispatch, answer }) {
+function Options({ question }) {
+  const { dispatch, answer } = useQuiz();
   const hasAnswered = answer !== null;
   return (
     <div className="options">
